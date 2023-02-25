51d623d49488bcdf26c9a81bbbfb3d6764214edd
Hanan Asyrawi Rivai <hasyrawi@gmail.com>
2023-02-25 18:23:40 +0700

    user service 100% covarage

--- internal/services/user_service_test.go
+++ internal/services/user_service_test.go
@@ -242,3 +242,96 @@ func TestUserService_DoLogin(t *testing.T) {
 		})
 	}
 }
+
+func TestUserService_DoGetUsers(t *testing.T) {
+
+	ur := mocks.NewUserRepository(t)
+
+	ur.On("GetUsers").Return([]*domain.User{
+		{
+			Id:       "1",
+			Name:     "any",
+			Username: "any",
+			Email:    "any",
+			Password: "any",
+		},
+	}, nil)
+
+	us := &UserService{
+		userRepo: ur,
+		config:   &config.Config{},
+	}
+
+	u, err := us.DoGetUsers()
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
+
+func TestUserService_DoGetUser(t *testing.T) {
+
+	ur := mocks.NewUserRepository(t)
+
+	ur.On("GetUser", "hanan").Return(&domain.User{Id: "1", Name: "any", Username: "any", Email: "any", Password: "any"}, nil)
+
+	us := &UserService{
+		userRepo: ur,
+		config:   &config.Config{},
+	}
+
+	u, err := us.DoGetUser("hanan")
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+
+}
+
+func TestUserService_DoDeleteUser(t *testing.T) {
+	ur := mocks.NewUserRepository(t)
+
+	ur.On("GetUserById", "1").Return(&domain.User{
+		Id:       "1",
+		Name:     "any",
+		Username: "any",
+		Email:    "any",
+		Password: "any",
+	}, nil)
+
+	ur.On("DeleteUser", "1").Return(nil)
+
+	us := &UserService{
+		userRepo: ur,
+		config:   &config.Config{},
+	}
+
+	err := us.DoDeleteUser("1")
+	assert.NoError(t, err)
+}
+
+func TestUserService_DoDeleteUser_WithNoUser(t *testing.T) {
+	ur := mocks.NewUserRepository(t)
+
+	// Why Delete Method No Mocked ? cus after this function has called the function has return
+	// So the delete method not executed
+	ur.On("GetUserById", "1").Return(nil, errors.New("User Not Found"))
+
+	us := &UserService{
+		userRepo: ur,
+		config:   &config.Config{},
+	}
+
+	err := us.DoDeleteUser("1")
+	assert.Error(t, err)
+}
+
+func TestNewUserServie(t *testing.T) {
+
+	ur := mocks.NewUserRepository(t)
+	us := &UserService{
+		userRepo: ur,
+		config:   &config.Config{},
+	}
+	assert.NotNil(t, us)
+
+	us2 := NewUserServie(ur, us.config)
+	assert.NotNil(t, us2)
+
+}
